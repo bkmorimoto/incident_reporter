d2e8f8a31fb879a1445731589504fa60e3f7737b
costolo <ben.costolo@gmail.com>
2015-04-03 15:39:36 -0400

    change limit of results

--- report_model.rb
+++ report_model.rb
@@ -4,16 +4,16 @@ require 'json'
 class Report
 
   def report_by_zip_code(zip_code)
-    response = JSON.parse(open("https://data.cityofnewyork.us/resource/erm2-nwe9.json?incident_zip=#{zip_code}").read, symbolize_names: true)
+    response = JSON.parse(open("https://data.cityofnewyork.us/resource/erm2-nwe9.json?incident_zip=#{zip_code}&$limit=25000").read, symbolize_names: true)
     hashify(response)
   end
 
   def hashify(input_hash)
     count_hash = {}
     input_hash.each do |entry|
-      count_hash[entry[:descriptor]] == nil ? count_hash[entry[:descriptor]] = 1 : count_hash[entry[:descriptor]] += 1
+     	count_hash[entry[:descriptor]] == nil ? count_hash[entry[:descriptor]] = 1 : count_hash[entry[:descriptor]] += 1
     end
-    count_hash.sort_by(&:last).reverse.each do |k, v|
+    count_hash.sort_by(&:last).each do |k, v|
       puts "#{k}: #{v}"
     end
   end
