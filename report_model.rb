e08223fadbb10ed63de9887e572a31e5092d9308
Ben Morimoto <bkmorimoto@gmail.com>
2015-04-03 15:32:00 -0400

    fix hashify for JSON stuff

--- report_model.rb
+++ report_model.rb
@@ -5,13 +5,13 @@ class Report
 
   def report_by_zip_code(zip_code)
     response = JSON.parse(open("https://data.cityofnewyork.us/resource/erm2-nwe9.json?incident_zip=#{zip_code}").read, symbolize_names: true)
-    p response
+    hashify(response)
   end
 
-  def hashify(count_hash)
-    count_hash.each do |entry|
-      count_hash[entry.descriptor] == nil ? count_hash[entry.descriptor] = 1 :
-        count_hash[entry.descriptor] += 1
+  def hashify(input_hash)
+    count_hash = {}
+    input_hash.each do |entry|
+      count_hash[entry[:descriptor]] == nil ? count_hash[entry[:descriptor]] = 1 : count_hash[entry[:descriptor]] += 1
     end
     count_hash.sort_by(&:last).reverse.each do |k, v|
       puts "#{k}: #{v}"
