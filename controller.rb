7bbda28d04b5f5af5d8e84393091f6213f2523fb
Alexander Mihalopoulos <amihalop@gmail.com>
2015-04-03 16:22:46 -0400

    Update controller for ip address"

--- controller.rb
+++ controller.rb
@@ -11,16 +11,27 @@ class Controller
   end
 
   def run
-    ip_address = open('http://whatismyip.akamai.com').read
-    zip_code = Geocoder.search(ip_address)[0].data["zip_code"].to_i
     get_zip_return_complaints
     quit_yes_or_no
   end
 
   def get_zip_return_complaints
     puts "Welcome to your personal NYC complaint finder. Want to find all complaints near you?"
-    puts "Enter your zip code"
-    zip_code = user_input
+    puts "Search complaints by IP Address or by zip code"
+    puts "Enter 'IP' or 'ZIP'"
+    answer = user_input.upcase
+    case answer
+    when "IP"
+      ip_address = open('http://whatismyip.akamai.com').read
+      zip_code = Geocoder.search(ip_address)[0].data["zip_code"].to_i
+      puts "Searching complaints for zip code: #{zip_code}"
+    when "ZIP"
+      puts "Enter your zip"
+      zip_code = user_input
+    else
+      puts "Did not recognize your input"
+      get_zip_return_complaints
+    end
     @data.report_by_zip_code(zip_code)
   end
 
