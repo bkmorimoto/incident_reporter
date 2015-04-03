284e842329ac1fa1683947f0f364ef5dc2da53a0
costolo <ben.costolo@gmail.com>
2015-04-03 15:21:56 -0400

    asasdf

--- report_model.rb
+++ report_model.rb
@@ -1,11 +1,12 @@
 class Report
 
   def report_by_zip_code(zip_code)
-    @response = JSON.parse(open("https://data.cityofnewyork.us/resource/erm2-nwe9.json?incident_zip=#{zip_code}").read, symbolize_names: true)
+    response = JSON.parse(open("https://data.cityofnewyork.us/resource/erm2-nwe9.json?incident_zip=#{zip_code}").read, symbolize_names: true)
+  	hashify(response)
   end
 
-  def hashify(order)
-	  @response.each do |entry|
+  def hashify(input_hash)
+	  input_hash.each do |entry|
 			count_hash[entry.descriptor] == nil ? count_hash[entry.descriptor] = 1 : 
 			count_hash[entry.descriptor] += 1
 		end
